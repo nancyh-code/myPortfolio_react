6288d4673577a6f00de15a1bd17277f552338036
Nancy Hernandez <hernandezyanezhotmail.com>
2022-04-21 14:39:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className='App-header'>
+        <h1>My portfolio</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
